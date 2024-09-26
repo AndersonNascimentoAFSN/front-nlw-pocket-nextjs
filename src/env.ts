cd940e0419fde39dd445e6086433d7b2022989a0
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-09-26 20:09:05 -0300

    fix: error no build da aplicação

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import z from 'zod'
 export const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string().url().default('http://localhost:3333'),
   NODE_ENV: z.string().default('development'),
+  NODE_ENV2: z.string().default('development'),
 })
 
 export const env = envSchema.parse(process.env)
